4685cc86c2aa9be6fb5f675618ba0e2b68f4917c
krgarcia42 <kyle.g75@yahoo.com>
2026-04-13 10:24:57 -0400

    Update test_events.py

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,1 +1,7 @@
+import unittest
+from src.messaging_service import send_message
 
+class TestEvents(unittest.TestCase):
+  def test_send_message(self):
+    result = send_message("test.topic", "123", {"data": "test"})
+    self.assertIsNone(result)
