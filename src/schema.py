5d24a0a889a455f5875f21777c0fe3b047f3ed5d
krgarcia42 <kyle.g75@yahoo.com>
2026-04-13 10:40:54 -0400

    Update schema.py

--- src/schema.py
+++ src/schema.py
@@ -1,6 +1,12 @@
+import uuid
+import time
+
 def create_event(topic, payload):
+  #fix: added missing imports (uuid & time) and included required fields (type & timestamp)
   return {
+    "type": "publish",
     "topic": topic,
-    "event_id": "12345",
-    "payload": payload
+    "event_id": (uuid.uuid4()),
+    "payload": payload,
+    "timestamp": time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime()) # ISO 8601 format
   }
