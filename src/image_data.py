7f061e56d1710880b6fc4eaf1c7c339c671df282
krgarcia42 <kyle.g75@yahoo.com>
2026-04-22 10:44:50 -0400

    Add hard-coded dataset for image annotations

---
+++ src/image_data.py
@@ -0,0 +1,13 @@
+# hard-coded dataset for 10 images
+IMAGE_DATASET = [
+    {"path": "forest_01.jpg", "boxes": [[10, 10, 50, 50]], "labels": ["tree", "bird"]},
+    {"path": "city_02.jpg", "boxes": [[100, 200, 150, 250]], "labels": ["car", "bus"]},
+    {"path": "ocean_03.jpg", "boxes": [[30, 40, 80, 90]], "labels": ["fish", "water"]},
+    {"path": "home_04.jpg", "boxes": [[5, 5, 20, 20]], "labels": ["cat", "couch"]},
+    {"path": "park_05.jpg", "boxes": [[45, 45, 95, 95]], "labels": ["dog", "person"]},
+    {"path": "street_06.jpg", "boxes": [[80, 80, 120, 120]], "labels": ["bicycle", "helmet"]},
+    {"path": "cafe_07.jpg", "boxes": [[15, 15, 60, 60]], "labels": ["coffee", "table"]},
+    {"path": "office_08.jpg", "boxes": [[50, 50, 100, 100]], "labels": ["laptop", "desk"]},
+    {"path": "sky_09.jpg", "boxes": [[0, 0, 100, 100]], "labels": ["cloud", "sun"]},
+    {"path": "kitchen_10.jpg", "boxes": [[25, 25, 75, 75]], "labels": ["plate", "fork"]}
+]
